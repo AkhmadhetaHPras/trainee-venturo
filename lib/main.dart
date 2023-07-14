02d2b8635a477ad392de7d27ebff4f90d80af5f2
AkhmadhetaHPras <akhmadhetahafid@gmail.com>
2023-07-14 09:07:53 +0700

    log with sentry

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:trainee/configs/pages/main_page.dart';
 import 'package:trainee/configs/routes/main_route.dart';
 import 'package:trainee/configs/themes/main_theme.dart';
@@ -12,7 +13,14 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-  runApp(const MyApp());
+  await SentryFlutter.init(
+    (options) {
+      options.dsn =
+          'https://54d36924361e4cbf86384996e2538227@o4505525100609536.ingest.sentry.io/4505525102051328';
+      options.tracesSampleRate = 1.0;
+    },
+    appRunner: () => runApp(const MyApp()),
+  );
 }
 
 class MyApp extends StatelessWidget {
--- lib/modules/features/counter/controllers/conter_controller.dart
+++ lib/modules/features/counter/controllers/conter_controller.dart
@@ -1,8 +1,19 @@
 import 'package:get/get.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 
 class ConterController extends GetxController {
   static ConterController get to => Get.find();
 
   var count = 0.obs;
-  increment() => count++;
+  increment() async {
+    try {
+      count++;
+      // throw Exception('Error occurred');
+    } catch (exception, stackTrace) {
+      await Sentry.captureException(
+        exception,
+        stackTrace: stackTrace,
+      );
+    }
+  }
 }
