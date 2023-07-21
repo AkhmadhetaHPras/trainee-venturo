c569c924abf5d2cde403da3e4c0c6726cf7e6ab6
AkhmadhetaHPras <akhmadhetahafid@gmail.com>
2023-07-21 16:01:07 +0700

    expand distance

--- lib/constants/commons/app_constants.dart
+++ lib/constants/commons/app_constants.dart
@@ -1,5 +1,5 @@
 class AppConstants {
   static const double locationLatitude = -8.246276525060397;
   static const double locationLongitude = 112.33661882207073;
-  static const double maximumDistance = 20;
+  static const double maximumDistance = 200000000;
 }
